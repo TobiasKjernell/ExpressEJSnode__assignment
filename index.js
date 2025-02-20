692a4087b56246793eae4daff879d9fc26bbeb84
DenVitaKaninen <tobiaskjernell@gmail.com>
2025-02-21 00:30:55 +0100

    port fix

--- index.js
+++ index.js
@@ -31,4 +31,4 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3030, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
