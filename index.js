ba918f6dc301f33d5c961c59a05b71300c60ca8a
DenVitaKaninen <tobiaskjernell@gmail.com>
2025-02-21 20:23:06 +0100

    vercel image path check - 2

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ app.use('/races', racesRouter);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
